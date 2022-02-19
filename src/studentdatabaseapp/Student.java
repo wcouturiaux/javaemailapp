943b313b3c0b0c15d4d9ec7b32f07f7bab842e90
Will C <wlccodes@gmail.com>
2022-02-18 21:32:48 -0500

    Add Student Database Package and Student Class

---
+++ src/studentdatabaseapp/Student.java
@@ -0,0 +1,4 @@
+package studentdatabaseapp;
+
+public class Student {
+}
---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,7 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+    //Ask how many users to add
+
+    //Create n number of new students
+}
