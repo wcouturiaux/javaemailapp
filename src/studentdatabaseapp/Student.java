73ebd7a5d0f7cc7830181597cccc80aae695aa4d
Will C <wlccodes@gmail.com>
2022-02-18 21:36:55 -0500

    Add student class properties

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,4 +1,22 @@
 package studentdatabaseapp;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String classStanding;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+    //Constructor: Prompt user to enter Name and year
+
+    //Generate an ID
+
+    //Enroll in Courses
+
+    //View Balance
+
+    //Pay Tuition
+
+    //Show Status
 }
