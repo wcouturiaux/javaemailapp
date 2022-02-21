29e3900af0fe045ce53e5b57ab9a9bc6ccd39637
Will C <wlccodes@gmail.com>
2022-02-20 20:24:03 -0500

    Store and print accounts from CSV

--- src/bankapp/BankApp.java
+++ src/bankapp/BankApp.java
@@ -1,12 +1,14 @@
 package bankapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankApp {
     public static void main(String[] args) {
-        String file = "/Users/WillC/javabyprojects/emailApp/NewBankAccounts.csv";
+        List<Account> accounts = new LinkedList<Account>();
 
         //Read CSV of customers
+        String file = "/Users/WillC/javabyprojects/emailApp/NewBankAccounts.csv";
         List<String[]> newAccountHolders = utilities.CSV.read(file);
         for (String[] accountHolder : newAccountHolders) {
             String name = accountHolder[0];
@@ -15,17 +17,22 @@ public class BankApp {
             double initDeposit = Double.parseDouble(accountHolder[3]);
             //System.out.println(name + " " + ssn + " " + accountType + " $" + initDeposit);
             if (accountType.equals("Savings")) {
-                System.out.println("OPEN A SAVINGS ACCOUNT");
+                accounts.add(new Savings(name, ssn, initDeposit));
+                //System.out.println("OPEN A SAVINGS ACCOUNT");
             }
             else if (accountType.equals("Checking")) {
-                System.out.println("OPEN A CHECKING ACCOUNT");
+                accounts.add(new Checking(name, ssn, initDeposit));
+                //System.out.println("OPEN A CHECKING ACCOUNT");
             }
             else {
                 System.out.println("Error reading account type");
             }
         }
         //Create Customer Accounts from CSV data
-
+        for (Account account:accounts) {
+            System.out.println("\n*********************");
+            account.showInfo();
+        }
         /* Used as unit tests during development
         Checking chkacct1 = new Checking("Tom Wilson", "3333333334", 1500);
         Savings savacct1 = new Savings("Jim Bob", "3333333331", 2200);
